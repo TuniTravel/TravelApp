456d985e4e2fdb62b05cde77d4c7d200a24025b2
Maram Zribi <129750651+MaramZribi@users.noreply.github.com>
2024-03-17 22:52:52 +0100

    ProjectCreation
    The project is created

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+      <Route index element={<IndexPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      </Route>
+    </Routes>
+    
+  )
+}
