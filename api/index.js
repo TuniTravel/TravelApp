14a73639c4de48fcbf22c79802fa17da9bc7f6e1
Maram Zribi <129750651+MaramZribi@users.noreply.github.com>
2024-03-19 19:42:14 +0100

    MongoUpdate
    The database is changed to TuniHike

--- api/index.js
+++ api/index.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5177',
+  origin: 'http://localhost:5173',
 }));
 
-mongoose.connect('mongodb+srv://Travel:sLvQvIZCTZYGFg4g@cluster0.uqeulul.mongodb.net/');
+mongoose.connect('mongodb+srv://Hike:oYxnRlROgpvVGoed@cluster0.geoeuuo.mongodb.net/');
 app.get('/test', (req, res) => {
   res.json('test ok');
 
@@ -30,8 +30,31 @@ app.post('/register', async (req, res) => {
 
 
 });
-//sLvQvIZCTZYGFg4g
+app.post('/login', async (req,res) => {
+ //mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+   /* const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }*/
+    res.json('found');
 
+  } else {
+    res.json('not found');
+  }
+});
+
+//oYxnRlROgpvVGoed
 // Your routes and other middleware here
 
 app.listen(4000);
