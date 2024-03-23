c48bf65b04b4a3369159fec84ced900cdee1fc72
Maram Zribi <129750651+MaramZribi@users.noreply.github.com>
2024-03-23 21:55:00 +0100

    TotalUP
    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,40 +5,26 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Layout from "./Layout";
 import axios from "axios";
-<<<<<<< HEAD
 import AccountPage from "./pages/AccountPage";
 axios.defaults.baseURL = "http://localhost:4000";
-=======
-axios.defaults.baseURL = 'http://localhost:4000';
-axios.defaults.withCredentials = true;
-import { UserContextProvider } from "./UserContext";
->>>>>>> origin
 export default function App() {
 
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account/:subpage" element={<AccountPage />} />
-        <Route path="/account/:subpage/:action" element={<AccountPage />} />
-      </Route>
-    </Routes>
-  );
-=======
+  
     <UserContextProvider>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage" element={<AccountPage />} />
+        <Route path="/account/:subpage/:action" element={<AccountPage />} />
+        <Route path="/account/places" element={<PlacesPage />} />
+        <Route path="/account/places/new" element={<PlacesPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
 
 
-  )
->>>>>>> origin
+  );
 }
